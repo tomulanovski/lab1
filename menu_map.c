a2b9b76a3d638ff1b6bbe39e270621f799f9a011
tomulanovski <63614033+tomulanovski@users.noreply.github.com>
2024-01-14 21:56:05 +0200

    Update menu_map.c

--- menu_map.c
+++ menu_map.c
@@ -39,3 +39,32 @@ char decrypt(char c) {
     }
     else return c;
 }
+struct fun_desc {
+    char *name;
+    char (*fun)(char);
+};
+
+void MenuLoop(){
+    char* carray = (char*)(malloc(5*sizeof(char)));
+    struct fun_desc menu[] = {
+            {"Get string", &my_get},
+            {"Print string", &cprt},
+            {"Print Hex", &xprt},
+            {"Encrypt", &encrypt},
+            {"Decrypt", &decrypt},
+            {NULL,NULL}
+    };
+    int bound = sizeof(menu) / (sizeof(menu[0]))-1;
+    int opChosen;
+    while (1) {
+        printf("Select operation from the following menu:\n");
+        for (int i = 0; menu[i].name != NULL; i++) {
+            printf("%d) %s\n", i, menu[i].name);
+        }
+    }
+}
+int main(int argc, char **argv)
+{
+    MenuLoop();
+    return 0;
+}
